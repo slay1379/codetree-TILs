3975424b9658ff29b2ac9e381f0f6b25ebf3073d
slay1379 <65012092+slay1379@users.noreply.github.com>
2024-09-23 19:15:10 +0900

    Title: 색깔 트리, Time: 1014ms, Memory: 38MB, Status: Wrong Answer - CodeTree

--- 240923/색깔 트리/color-tree.py
+++ 240923/색깔 트리/color-tree.py
@@ -1,21 +1,25 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(100000)
+from itertools import combinations
+from collections import deque
+from collections import defaultdict
+import heapq
 
 class Node:
-    __slots__ = ['value','children','color','max_depth','have_color','parent']
-    def __init__(self, value):
+    __slots__ = ['value','children','color','max_depth','last_update','have_color']
+    def __init__(self,value):
         self.value = value
         self.children = []
         self.color = None
         self.max_depth = None
         self.have_color = set()
-        self.parent = None  # 부모 노드를 기록
 
 nodes = {}
 root_nodes = {}
 
-def insert_node(m_id, p_id, color, max_depth):
+def insert_node(m_id,p_id,color,max_depth):
+    global rootId
     if p_id == -1:
         root = Node(m_id)
         root.color = color
@@ -24,34 +28,34 @@ def insert_node(m_id, p_id, color, max_depth):
         nodes[m_id] = root
     else:
         parent = nodes.get(p_id)
-        if parent is None or len(parent.children) >= parent.max_depth - 1:
+        if parent is None:
+            return
+        if len(parent.children) >= parent.max_depth-1:
             return
         child = Node(m_id)
         child.color = color
         child.max_depth = max_depth
-        child.parent = parent  # 부모 노드 기록
         nodes[m_id] = child
         parent.children.append(child)
 
-def change_color(node, color):
+def change_color(node,color):
     node.color = color
     for child in node.children:
-        change_color(child, color)
+        change_color(child,color)
 
 def cal_have_color(node):
-    if not node.children:  # 리프 노드인 경우
-        node.have_color = {node.color}
-    else:
-        node.have_color = {node.color}
-        for child in node.children:
-            cal_have_color(child)
-            node.have_color.update(child.have_color)
+    node.have_color = set()
+    node.have_color.add(node.color)
+    for child in node.children:
+        cal_have_color(child)
+        node.have_color.update(child.have_color)
 
 def cal_value(node):
     global total_value
-    total_value += len(node.have_color) ** 2
+    total_value += len(node.have_color) * len(node.have_color)
     for child in node.children:
         cal_value(child)
+
 
 Q = int(input())
 for _ in range(Q):
@@ -64,7 +68,7 @@ for _ in range(Q):
         m_id, color = oper_input[1:]
         node = nodes.get(m_id)
         if node:
-            change_color(node, color)
+            change_color(node,color)
     elif oper == 300:
         m_id = oper_input[1]
         node = nodes.get(m_id)
@@ -75,8 +79,10 @@ for _ in range(Q):
     else:
         sum_total = 0
         for root in root_nodes.values():
-            cal_have_color(root)
+            rootId = root.value
+            node = nodes.get(rootId)
+            cal_have_color(node)
             total_value = 0
-            cal_value(root)
+            cal_value(node)
             sum_total += total_value
         print(sum_total)
